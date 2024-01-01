bf6f9bcddae5147eeae313b53abd9dcab46d610e
Ramy Tanios <ramy.tanios@gmail.com>
2024-01-01 23:04:22 +0100

    separate options

--- home/default.nix
+++ home/default.nix
@@ -4,32 +4,47 @@ with lib;
 let
 
   cfg = config.colorschemes.kauz;
+  isFish = cfg.fish.enable;
+  isKitty = cfg.kitty.enable;
+  isTmux = cfg.tmux.enable;
+  isNeovim = cfg.neovim.enable;
 
 in {
 
   options.colorschemes.kauz = {
-    enable = mkEnableOption "a colorscheme for neovim, kitty, fish and tmux";
+    # fish
+    fish.enable = mkEnableOption "fish colorscheme integration";
+
+    # kitty 
+    kitty.enable = mkEnableOption "tmux colorscheme integration";
+
+    # tmux 
+    tmux.enable = mkEnableOption "tmux colorscheme integration";
+
+    #neovim
+    neovim.enable = mkEnableOption "neovim colorscheme integration";
   };
 
-  config = mkIf cfg.enable {
+  config = mkIf isKitty {
 
     programs.kitty.extraConfig = ''
       include ${pkgs.kauz-kitty}/kauz.conf
     '';
-
+  } // mkIf isTmux {
     programs.tmux.extraConfig = builtins.readFile "${pkgs.kauz-tmux}/kauz.tmux";
-
+  } // mkIf isFish {
     programs.fish.plugins = [{
       name = "kauz-fish";
       inherit (pkgs.kauz-fish) src;
     }];
+  } // mkIf isNeovim {
 
     programs.neovim.plugins = [ pkgs.kauz-nvim ];
 
     programs.neovim.extraLuaConfig = ''
       vim.cmd.colorscheme("kauz")
     '';
-
   };
 
 }
+
