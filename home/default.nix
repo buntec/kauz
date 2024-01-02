bdf08c7f24bb7f735b5637f369f0b2fe07eba139
Ramy Tanios <ramy.tanios@gmail.com>
2024-01-02 13:13:25 +0100

    use `mkIf`s in hm config

--- home/default.nix
+++ home/default.nix
@@ -4,55 +4,45 @@ with lib;
 let
 
   cfg = config.kauz;
+
   supported = [ "fish" "kitty" "neovim" "tmux" ];
-  allEnabled = builtins.filter (s: cfg.${s}.enable) supported;
+
   capitalizeFirst = s:
     lib.toUpper (builtins.substring 0 1 s)
     + builtins.substring 1 (lib.stringLength s) s;
 
+  isFish = cfg.fish.enable;
+  isKitty = cfg.kitty.enable;
+  isTmux = cfg.tmux.enable;
+  isNeovim = cfg.neovim.enable;
+
 in {
 
-  options.kauz = builtins.listToAttrs (builtins.map (s: {
+  options.kauz = builtins.listToAttrs (map (s: {
     name = s.enable;
     value = mkEnableOption
       "Whether to enable the colorscheme for ${capitalizeFirst s}";
   }) supported);
 
-  config = builtins.listToAttrs (builtins.map (enabled: {
-    name = "programs.${enabled}";
-    value =
+  config = mkIf isFish {
+    programs.fish.plugins = [{
+      name = "kauz-fish";
+      inherit (pkgs.kauz-fish) src;
+    }];
+  } // mkIf isKitty {
+    programs.kitty.extraConfig = ''
+      include ${pkgs.kauz-kitty}/kauz.conf
+    '';
+  } // mkIf isTmux {
+    programs.tmux = ''
+      builtins.readFile "${pkgs.kauz-tmux}/kauz.tmux";
+    '';
+  } // mkIf isNeovim {
+    programs.neovim.plugins = [ pkgs.kauz-nvim ];
+    programs.neovim.extraLuaConfig = ''
+      vim.cmd.colorscheme("kauz");
+    '';
+  };
 
-      if enabled == "kitty" then {
-        extraConfig = ''
-          include ${pkgs.kauz-kitty}/kauz.conf
-        '';
-      }
-
-      else if enabled == "tmux" then {
-        extraConfig = ''
-          builtins.readFile "${pkgs.kauz-tmux}/kauz.tmux";
-        '';
-
-      }
-
-      else if enabled == "fish" then {
-        plugins = [{
-          name = "kauz-fish";
-          inherit (pkgs.kauz-fish) src;
-        }];
-
-      }
-
-      else if enabled == "neovim" then {
-        plugins = [ pkgs.kauz-nvim ];
-        extraLuaConfig = ''
-          vim.cmd.colorscheme("kauz");
-        '';
-      }
-
-      else
-        { }; # should never happen
-
-  }) allEnabled);
 }
 
