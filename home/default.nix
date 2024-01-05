0787e60bfc26996b3c943f3ac631b35336ed43e1
Christoph Bunte <christophbunte@gmail.com>
2024-01-05 18:16:45 +0100

    attempt to fix `config` section

--- home/default.nix
+++ home/default.nix
@@ -2,7 +2,6 @@
 with lib;
 
 let
-
   cfg = config.kauz;
 
   isFish = cfg.fish.enable;
@@ -23,23 +22,29 @@ in {
       mkEnableOption "Whether to enable the Kauz colorscheme for Neovim";
   };
 
-  config = mkIf isFish {
-    programs.fish.plugins = [{
-      name = "kauz-fish";
-      inherit (pkgs.kauz-fish) src;
-    }];
-  } // mkIf isKitty {
-    programs.kitty.extraConfig = ''
-      include ${pkgs.kauz-kitty}/kauz.conf
-    '';
-  } // mkIf isTmux {
-    programs.tmux.extraConfig = builtins.readFile "${pkgs.kauz-tmux}/kauz.tmux";
-  } // mkIf isNeovim {
-    programs.neovim.plugins = [ pkgs.kauz-nvim ];
-    programs.neovim.extraLuaConfig = ''
-      vim.cmd.colorscheme("kauz")
-    '';
-  };
+  config = mkMerge [
+    (mkIf isFish {
+      programs.fish.plugins = [{
+        name = "kauz-fish";
+        inherit (pkgs.kauz-fish) src;
+      }];
+    })
+    (mkIf isKitty {
+      programs.kitty.extraConfig = ''
+        include ${pkgs.kauz-kitty}/kauz.conf
+      '';
+    })
+    (mkIf isTmux {
+      programs.tmux.extraConfig =
+        builtins.readFile "${pkgs.kauz-tmux}/kauz.tmux";
+    })
+    (mkIf isNeovim {
+      programs.neovim.plugins = [ pkgs.kauz-nvim ];
+      programs.neovim.extraLuaConfig = ''
+        vim.cmd.colorscheme("kauz")
+      '';
+    })
+  ];
 
 }
 
