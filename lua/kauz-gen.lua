02f769e271c80552836170be2c0e322f4a231a71
Christoph Bunte <christophbunte@gmail.com>
2025-03-01 18:32:26 +0100

    add base24 versions

--- lua/kauz-gen.lua
+++ lua/kauz-gen.lua
@@ -1,20 +1,27 @@
--- Generator for the Kauz colorscheme.
+-- Generator for the Kauz color scheme.
 --
 -- Run this file from neovim using `:luafile %`. Requires the mini.base16 and mini.colors plugins!
 
--- From the (Base16 Styling Guidelines)[https://github.com/tinted-theming/home/blob/main/styling.md]:
+-- From the (Base16 styling guidelines)[https://github.com/tinted-theming/home/blob/main/styling.md]:
 --
 -- Colors base00 to base07 are typically variations of a shade and run from darkest to lightest for dark themes.
 -- These colors are used for foreground and background, status bars, line highlighting and such.
 -- Colors base08 to base0F are typically individual colors used for types, operators, names and variables.
 -- In order to create a dark scheme, colors base00 to base07 should span from dark to light.
 -- For a light scheme, these colours should span from light to dark.
+--
+-- See also the (Base24 styling guidelines)[https://github.com/tinted-theming/base24/blob/main/styling.md].
 
 -- Notes:
 -- oklch: l = lightness, c = chroma (saturation), h = hue (angle)
 
 -- mini.colors gives us conversion from oklch to hex
 local mc = require "mini.colors"
+
+-- toggle this to preview dark/light variants
+local preview_dark = false
+
+-- Kauz theme definitions
 
 -- dark/light
 local h0 = 220.0 -- hue of 00--07
@@ -27,6 +34,7 @@ local c1 = 5.5 -- chroma of 08--0F
 local l0 = 20.0 -- lightness of darkest background
 local dl = 10.0 -- lightness increments
 local l1 = 80.0 -- lightness of 08--0F
+local l2 = 100.0 -- lightness of 12--17
 
 Kauz_palette_dark = {
   base00 = mc.convert({ l = l0 + 0 * dl, c = c0, h = h0 }, "hex"), -- default background
@@ -46,6 +54,15 @@ Kauz_palette_dark = {
   base0D = mc.convert({ l = l1, c = c1, h = h1 + 5 * dh }, "hex"), -- functions, methods, ...
   base0E = mc.convert({ l = l1, c = c1, h = h1 + 6 * dh }, "hex"), -- keywords, storage, diff changed, ...
   base0F = mc.convert({ l = l1, c = c1, h = h1 + 7 * dh }, "hex"), -- deprecated, opening/closing tags, ...
+
+  base10 = mc.convert({ l = l0 - 1 * dl, c = c0, h = h0 }, "hex"), -- darker background (darker than 00)
+  base11 = mc.convert({ l = l0 - 2 * dl, c = c0, h = h0 }, "hex"), -- the darkest background (even darker than 10)
+  base12 = mc.convert({ l = l2, c = c1, h = h1 + 0 * dh }, "hex"), -- brighter 08
+  base13 = mc.convert({ l = l2, c = c1, h = h1 + 2 * dh }, "hex"), -- brigher 0A
+  base14 = mc.convert({ l = l2, c = c1, h = h1 + 3 * dh }, "hex"), -- brighter 0B
+  base15 = mc.convert({ l = l2, c = c1, h = h1 + 4 * dh }, "hex"), -- brighter 0C
+  base16 = mc.convert({ l = l2, c = c1, h = h1 + 5 * dh }, "hex"), -- brighter 0D
+  base17 = mc.convert({ l = l2, c = c1, h = h1 + 6 * dh }, "hex"), -- brighter 0E
 }
 
 -- light
@@ -55,10 +72,11 @@ c1 = 5.5 -- chroma of 08--0F
 l0 = 100.0 -- lightness of lightest background
 dl = -10.0 -- lightness decrements of 00--07
 l1 = 60.0 -- lightness of 08--0F
+l2 = 80.0 -- lightness of 12--17
 
 Kauz_palette_light = {
-  base00 = mc.convert({ l = l0 + 0 * dl, c = c0, h = h0 }, "hex"),
-  base01 = mc.convert({ l = l0 + 1 * dl, c = c0, h = h0 }, "hex"),
+  base00 = mc.convert({ l = l0 + 0 * dl, c = c0, h = h0 }, "hex"), -- default background
+  base01 = mc.convert({ l = l0 + 1 * dl, c = c0, h = h0 }, "hex"), -- darker background
   base02 = mc.convert({ l = l0 + 2 * dl, c = c0, h = h0 }, "hex"),
   base03 = mc.convert({ l = l0 + 3 * dl, c = c0, h = h0 }, "hex"),
   base04 = mc.convert({ l = l0 + 4 * dl, c = c0, h = h0 }, "hex"),
@@ -74,7 +92,18 @@ Kauz_palette_light = {
   base0D = mc.convert({ l = l1, c = c1, h = h1 + 5 * dh }, "hex"),
   base0E = mc.convert({ l = l1, c = c1, h = h1 + 6 * dh }, "hex"),
   base0F = mc.convert({ l = l1, c = c1, h = h1 + 7 * dh }, "hex"),
+
+  base10 = mc.convert({ l = l0 - 1 * dl, c = c0, h = h0 }, "hex"), -- lighter background (lighter than 00)
+  base11 = mc.convert({ l = l0 - 2 * dl, c = c0, h = h0 }, "hex"), -- the lightest background (even lighter than 10)
+  base12 = mc.convert({ l = l2, c = c1, h = h1 + 0 * dh }, "hex"), -- brighter 08
+  base13 = mc.convert({ l = l2, c = c1, h = h1 + 2 * dh }, "hex"), -- brigher 0A
+  base14 = mc.convert({ l = l2, c = c1, h = h1 + 3 * dh }, "hex"), -- brighter 0B
+  base15 = mc.convert({ l = l2, c = c1, h = h1 + 4 * dh }, "hex"), -- brighter 0C
+  base16 = mc.convert({ l = l2, c = c1, h = h1 + 5 * dh }, "hex"), -- brighter 0D
+  base17 = mc.convert({ l = l2, c = c1, h = h1 + 6 * dh }, "hex"), -- brighter 0E
 }
+
+-- Helper functions
 
 -- Simple string interpolation.
 --
@@ -90,15 +119,15 @@ local function template(str, table)
   )
 end
 
-local function write(str, fileName)
-  print("[write] base16/" .. fileName)
-  vim.fn.mkdir(vim.fs.dirname("base16/" .. fileName), "p")
-  local file = assert(io.open("base16/" .. fileName, "w"))
+local function write(str, baseDir, fileName)
+  print("[write] " .. baseDir .. "/" .. fileName)
+  vim.fn.mkdir(vim.fs.dirname(baseDir .. "/" .. fileName), "p")
+  local file = assert(io.open(baseDir .. "/" .. fileName, "w"))
   file:write(str)
   file:close()
 end
 
-local function write_scheme_to_yaml()
+local function write_yaml_base16()
   local yaml_template = [[
 system: "base16"
 name: "Kauz"
@@ -121,11 +150,63 @@ palette:
   base0D: "${base0D}"
   base0E: "${base0E}"
   base0F: "${base0F}"
-
 ]]
 
-  write(template(yaml_template, vim.tbl_extend("error", Kauz_palette_dark, { variant = "dark" })), "kauz-dark.yml")
-  write(template(yaml_template, vim.tbl_extend("error", Kauz_palette_light, { variant = "light" })), "kauz-light.yml")
+  write(
+    template(yaml_template, vim.tbl_extend("error", Kauz_palette_dark, { variant = "dark" })),
+    "base16",
+    "kauz-dark.yml"
+  )
+  write(
+    template(yaml_template, vim.tbl_extend("error", Kauz_palette_light, { variant = "light" })),
+    "base16",
+    "kauz-light.yml"
+  )
+end
+
+local function write_yaml_base24()
+  local yaml_template = [[
+system: "base24"
+name: "Kauz"
+author: "buntec"
+variant: "${variant}"
+palette:
+  base00: "${base00}"
+  base01: "${base01}"
+  base02: "${base02}"
+  base03: "${base03}"
+  base04: "${base04}"
+  base05: "${base05}"
+  base06: "${base06}"
+  base07: "${base07}"
+  base08: "${base08}"
+  base09: "${base09}"
+  base0A: "${base0A}"
+  base0B: "${base0B}"
+  base0C: "${base0C}"
+  base0D: "${base0D}"
+  base0E: "${base0E}"
+  base0F: "${base0F}"
+  base10: "${base10}"
+  base11: "${base11}"
+  base12: "${base12}"
+  base13: "${base13}"
+  base14: "${base14}"
+  base15: "${base15}"
+  base16: "${base16}"
+  base17: "${base17}"
+]]
+
+  write(
+    template(yaml_template, vim.tbl_extend("error", Kauz_palette_dark, { variant = "dark" })),
+    "base24",
+    "kauz-dark.yml"
+  )
+  write(
+    template(yaml_template, vim.tbl_extend("error", Kauz_palette_light, { variant = "light" })),
+    "base24",
+    "kauz-light.yml"
+  )
 end
 
 function Kauz_switch_to_light()
@@ -140,10 +221,6 @@ function Kauz_switch_to_dark()
   }
 end
 
-vim.api.nvim_create_user_command("KauzWriteSchemeToYaml", function()
-  write_scheme_to_yaml()
-end, { desc = "write Kauz scheme to yaml files" })
-
 local function run_current_lua_file()
   local file = vim.fn.expand "%"
   if file ~= "" then
@@ -153,15 +230,24 @@ local function run_current_lua_file()
   end
 end
 
-local kauz_augroup = vim.api.nvim_create_augroup("kauz", { clear = true })
+if preview_dark then
+  Kauz_switch_to_dark()
+else
+  Kauz_switch_to_light()
+end
+
+vim.api.nvim_create_user_command("KauzWriteSchemeToYaml", function()
+  write_yaml_base16()
+  write_yaml_base24()
+end, { desc = "write Kauz scheme to yaml files" })
+
+local group = vim.api.nvim_create_augroup("kauz", { clear = true })
 
 vim.api.nvim_create_autocmd({ "BufWritePost" }, {
   pattern = "kauz-gen.lua",
   desc = "reload colorscheme on save",
   callback = function()
     run_current_lua_file()
-    Kauz_switch_to_light()
-    -- Kauz_switch_to_dark()
   end,
-  group = kauz_augroup,
+  group = group,
 })
